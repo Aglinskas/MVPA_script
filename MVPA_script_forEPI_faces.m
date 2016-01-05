96b4b9b5101a5b743456afd18838f721e968b35a
Aglinskas <aidas.aglinskas@gmail.com>
2016-01-05 17:24:07 +0100

    prep for monument decoding

--- MVPA_script_forEPI_faces.m
+++ MVPA_script_forEPI_faces.m
@@ -2,26 +2,32 @@
 %if db_mode == 0;
 
 %% Parameters
-subID = 4 % which subject
+subID = 6 % which subject
 nsess = 5; % how many sessions
 
 
-
+plotting = 0
 %% Directories
 %mask_fn='/Volumes/Aidas_HDD/MRI_data/S1/Analysis/mask.nii';
 mask_fn= sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/mask.nii',subID); % brain mask used for searchlight 
 addpath('/Users/aidas_el_cap/Documents/MATLAB/spm12/toolbox/marsbar/'); %marsbar dir keeps disappearing re-add it just in case
-mvpadir = '/Volumes/Aidas_HDD/MRI_data/MVPA_analyses/' % where to save stacked scans and outpt files
+mvpadir = '/Volumes/Aidas_HDD/MRI_data/MVPA_analyses/'; % where to save stacked scans and outpt files
 
-roi_path = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/face_selective_roi.mat',subID); load(roi_path); %FFAish_roi.mat'; load(roi_path);% load to roi object
+roi_path = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/FFA_ROI_roi.mat',subID); load(roi_path); %FFAish_roi.mat'; load(roi_path);% load to roi object
 % ^ FFA ROI used for plotting
 
 %% Load myTrials, plot and prep
-load(sprintf('/Volumes/Aidas_HDD/MRI_data/Other/myTrials3456_processed/S%d_Results.mat',subID))
+load(sprintf('/Volumes/Aidas_HDD/MRI_data/Other/myTrials3456_processed/S%d_Results.mat',subID));
 
+if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') == 2
+    disp('Stacked scans found, skipping plotting and stacking')
+end
+if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') ~= 2
 myTrials = MakeTRs2_faces(myTrials);
-clf
-hold on
+
+if plotting == 1
+clf;
+hold on;
 disp('preparing for plotting')
 for i = 1:nsess
     %hold on
@@ -29,13 +35,15 @@ for i = 1:nsess
     xlabel(sprintf('Session %d',i))
     drawnow
     P = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Functional/Sess%d/f50hz_swradata.nii',subID,i);
-   mY = get_marsy(roi, P, 'mean'); % extract data into marsy data object
-y(:,i) = summary_data(mY);% get summary time course(s) % needs marsy object as input
-plot(y(:,i))
+    mY = get_marsy(roi, P, 'mean'); % extract data into marsy data object   
+    % get_marsy_1 <- pure magic 
+y = summary_data(mY);% get summary time course(s) % needs marsy object as input
+plot(y)
 drawnow
 end
 disp('done')
-
+end
+end
 % Done preparing and plotting at this point.
 
 % %for plotting
@@ -56,24 +64,28 @@ disp('done')
 %filtered_data = '/Volumes/Aidas_HDD/MRI_data/S1/functional/Sess1/f50hz_wrdata.nii';
 if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') == 2 % if stacked scans exist
     load(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)))
-    disp('previously stacked scans found, loading')
+    disp('previously stacked scans loaded')
 else 
     disp('stacking scans')
-scans = find([myTrials.name_ID] ~= 0);
+%scans = find([myTrials.name_ID] ~= 0);
+scans = find([myTrials.name_ID] ~= 0 & [myTrials.blockNum] ~= 14);
 for ln = scans
     disp(ln)
     %hold on
     %filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S2/Functional/Sess%d/f50hz_swradata.nii',target_EPIs(ln,3));
     filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Functional/Sess%d/f50hz_swradata.nii',subID,myTrials(ln).fmriRun);
-    single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).blockNum * myTrials(ln).fmriRun, 'volumes', [myTrials(ln).TR myTrials(ln).TR + 1]);
-    subplot(4,1,myTrials(ln).fmriRun)
-    plot(myTrials(ln).TR,mean(single_scan.samples),'r*')
+    single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).blockNum, 'volumes',myTrials(ln).TR);
+    %subplot(4,1,myTrials(ln).fmriRun)
+    %plot(myTrials(ln).TR,mean(single_scan.samples),'r*')
     %drawnow
   if ln == 1
     all_scans=single_scan;
     else
     all_scans = cosmo_stack({all_scans,single_scan});
-  end %ends ln == 1 if statement
+  end
+  single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).blockNum, 'volumes', myTrials(ln).TR + 1);
+  all_scans = cosmo_stack({all_scans,single_scan});
+  %ends ln == 1 if statement
     %disp(['stacking scans ' num2str(length(all_scans.samples(:,1))) ' / ' num2str(length(scans)
 end
 disp('saving stacked scans')
@@ -112,7 +124,7 @@ opt=struct();
 opt.classifier=@cosmo_classify_lda;
 disp('opt.partitions')
 opt.partitions=cosmo_nchoosek_partitioner(all_scans,1);
-disp('balancing paritions')
+%disp('balancing paritions')
 %opt.partitions_bal = cosmo_balance_partitions(opt.partitions,all_scans); %added to balance partitions, otherwise cosmo doesnt like it
 %opt.partitions = opt.partitions_bal 
 %disp('partitions set and balanced, searchlight is up')
@@ -120,14 +132,13 @@ disp('balancing paritions')
 % define spherical neighborhood with radius of 3 voxels
 
 % Run the searchlight with a 3 voxel radius
-corr_results=cosmo_searchlight(all_scans,nbrhood,measure,opt);
-corr_results.samples=corr_results.samples-(1/15);
+corr_results=cosmo_searchlight(all_scans,nbrhood,measure,opt); % ERRORS HERE
+corr_results.samples=corr_results.samples-(1/40);
 %% save and exit
-output_fn=fullfile(mvpadir,sprintf('Sub%d_MVPA_results',subID));
+output_fn=fullfile(mvpadir,sprintf('Sub%d_MVPA_results.nii',subID));
 cosmo_map2fmri(corr_results, output_fn);
 % output_fn1 = fullfile(pwd,[ datestr(datetime) '_MVPA_outFile.nii'])
 % try
 % cosmo_map2fmri(corr_results, output_fn1);
 % catch
 %     cosmo_map2fmri(corr_results, output_fn);
-% end
