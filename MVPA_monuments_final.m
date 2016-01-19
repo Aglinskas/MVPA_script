ad186e5d9934e4eadff8688c8508aa1f8ee06475
Aglinskas <aidas.aglinskas@gmail.com>
2016-01-19 11:04:53 +0100

    Improvements to face script

--- MVPA_monuments_final.m
+++ MVPA_monuments_final.m
@@ -1,17 +1,16 @@
 %db_mode = 0; % debugger skips some of the initial steps and instead loads stuff that usually would be computer from a .mat file
 %if db_mode == 0;
-description = 'zscored per block, only good faces, only single scan loaded, 6 second hrf offset'
-%% Parameters 
-subID = 6 % which subject
+
+%% Parameters
+for subID = [4 5 6] % which subject
 nsess = 5; % how many sessions
+
 plotting = 0
 %% Directories
 %mask_fn='/Volumes/Aidas_HDD/MRI_data/S1/Analysis/mask.nii';
 %mask_fn= sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/mask.nii',subID); % brain mask used for searchlight 
 %mask_fn = '/Volumes/Aidas_HDD/MRI_data/MVPA_analyses/MVP_mask_for_MVPA.nii' % ROI-ish mask
-%mask_fn = '/Volumes/Aidas_HDD/MRI_data/S4/Analysis2/mask.nii'
-mask_fn = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis2/mask.nii',subID)
-%mask_fn = '/Users/aidas_el_cap/Desktop/MEGA_ROI_s4.nii'
+mask_fn = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/mask.nii',subID)
 %addpath('/Users/aidas_el_cap/Documents/MATLAB/spm12/toolbox/marsbar/'); %marsbar dir keeps disappearing re-add it just in case
 mvpadir = '/Volumes/Aidas_HDD/MRI_data/MVPA_analyses/'; % where to save stacked scans and outpt files
 
@@ -20,14 +19,17 @@ mvpadir = '/Volumes/Aidas_HDD/MRI_data/MVPA_analyses/'; % where to save stacked 
 
 %% Load myTrials, plot and prep
 load(sprintf('/Volumes/Aidas_HDD/MRI_data/Other/myTrials3456_processed/S%d_Results.mat',subID));
-myTrials = MakeTRs2_faces(myTrials);
+myTrials = MakeTRs2_facesvmonuments(myTrials);
+ffc = find([myTrials.name_ID] > 0);
+[myTrials(ffc).name_ID] = deal(1);
+
 % if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') == 2
 %     disp('Stacked scans found, skipping plotting and stacking')
 % end
 % if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') ~= 2
-% 
-% 
-% 
+% myTrials = MakeTRs2_faces(myTrials);
+
+
 % try
 % roi_path = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Analysis/FFA_ROI_roi.mat',subID); load(roi_path); %FFAish_roi.mat'; load(roi_path);% load to roi object
 % end
@@ -67,51 +69,33 @@ myTrials = MakeTRs2_faces(myTrials);
 %SPM.xY.P(1:3,:) first three lines
 %target_EPIs =;
 %load('/Volumes/Aidas_HDD/MRI_data/S2/fixed_target_EPIs.mat')
-
-%filtered_data = '/Volumes/Aidas_HDD/MRI_data/S1/functional/Sess1/f50hz_wrdata.nii';
+% 
+% %filtered_data = '/Volumes/Aidas_HDD/MRI_data/S1/functional/Sess1/f50hz_wrdata.nii';
 % if exist(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'file') == 2 % if stacked scans exist
 %     load(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)))
 %     disp('previously stacked scans loaded')
 % else 
-%     disp('stacking scans')
+    disp('stacking scans')
 %scans = find([myTrials.name_ID] ~= 0);
+%scans = find([myTrials.name_ID] ~= 0 & [myTrials.blockNum] ~= 14);
+scans = find([myTrials.blockNum] == 15 | [myTrials.blockNum] == 16 | [myTrials.blockNum] == 17 | [myTrials.blockNum] == 1 | [myTrials.blockNum] == 2 | [myTrials.blockNum] == 3)
 %%
-% fix skipped answers
-for i = 1 : length(myTrials)
-if isempty(myTrials(i).resp)
-myTrials(i).resp = 0
-end
-end
-%%
-bad_faces = [[myTrials(find([myTrials.blockNum] == 7 & [myTrials.resp] == 2)).name_ID]',
-[myTrials(find([myTrials.blockNum] == 11 & [myTrials.resp] == 4)).name_ID]',
-[myTrials(find([myTrials.blockNum] == 14 & [myTrials.resp] == 4)).name_ID]',
-[myTrials(find([myTrials.blockNum] == 2 & [myTrials.resp] == 4)).name_ID]'
-[myTrials(find([myTrials.blockNum] == 9 & [myTrials.resp] == 4)).name_ID]'];
-
-
-%legacy
-scans = find([myTrials.name_ID] ~= 0 & [myTrials.blockNum] ~= 14);
-%only good faces
-% scans = find([myTrials.name_ID] ~= 0 & [myTrials.blockNum] ~= 14 & ismember([myTrials.name_ID],bad_faces) ~= 1);
-% %%
 for ln = scans
     disp(ln)
     %hold on
     %filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S2/Functional/Sess%d/f50hz_swradata.nii',target_EPIs(ln,3));
-    %filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Functional/Sess%d/f50hz_swradata.nii',subID,myTrials(ln).fmriRun);
-    filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Functional/Sess%d/f50hz_rrdata.nii',subID,myTrials(ln).fmriRun);
-    single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).blockNum, 'volumes',myTrials(ln).TR);
+    filename = sprintf('/Volumes/Aidas_HDD/MRI_data/S%d/Functional/Sess%d/swradata.nii',subID,myTrials(ln).fmriRun);
+    single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).fmriRun, 'volumes',myTrials(ln).TR);
     %subplot(4,1,myTrials(ln).fmriRun)
     %plot(myTrials(ln).TR,mean(single_scan.samples),'r*')
     %drawnow
-  if ln == 1
+  if ln == scans(1)
     all_scans=single_scan;
     else
     all_scans = cosmo_stack({all_scans,single_scan});
   end
-  %single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).blockNum, 'volumes', myTrials(ln).TR + 1);
-  %all_scans = cosmo_stack({all_scans,single_scan});
+  single_scan = cosmo_fmri_dataset(filename,'mask', mask_fn,'targets',myTrials(ln).name_ID, 'chunks', myTrials(ln).fmriRun, 'volumes', myTrials(ln).TR + 1);
+  all_scans = cosmo_stack({all_scans,single_scan});
   %ends ln == 1 if statement
     %disp(['stacking scans ' num2str(length(all_scans.samples(:,1))) ' / ' num2str(length(scans)
 end
@@ -121,20 +105,14 @@ save(fullfile(mvpadir,sprintf('MVPA_stacked_scans_sub%d.mat',subID)),'all_scans'
 disp('saved')
 
 %% Z Scoring
-% first way, grab task in every run, zscore
-
-raw_all_scans = all_scans; %back of of raw scores;
-tasks = unique(all_scans.sa.chunks); %loads the tasks that are used in chunks
+% 1 way, grab task in every run, zscore
+raw_all_scans = all_scans; %
+tasks = unique(all_scans.sa.chunks);
 for p = 1 : length(tasks);
-tinx = find(all_scans.sa.chunks == p); %finds trials belongng to that task in all runs
+tinx = find(all_scans.sa.chunks == p);
 all_scans.samples(tinx,:) = zscore(all_scans.samples(tinx,:),[],1);
 end
 
-%per block z scoring
-% for c = 1:8:length(all_scans.sa.chunks) 
-%     all_scans.samples(c:c+7) = zscore(all_scans.samples(c:c+7),[],1);
-% end
-%Z scoring 2
 %%
 %    %% delete expanded scans
 %    if delete_expanded_scans == 1
@@ -147,8 +125,7 @@ end
 
 %exist(sprintf('MVPA_stacked_scans_sub%d.mat',subID),'file') %checks if
 %stacked scans already exist
-
-
+ 
 %  end%end of debuf if
 %%
 % if db_mode == 1
@@ -184,23 +161,15 @@ opt.partitions=cosmo_nchoosek_partitioner(all_scans,1);
 
 % Run the searchlight with a 3 voxel radius
 %%
-corr_results=cosmo_searchlight(all_scans,nbrhood,measure,opt); %
-%corr_results.samples=corr_results.samples-(1/40);
-corr_results.samples=corr_results.samples-(1/length(unique(all_scans.sa.targets)));
-
-r{1,1} = 'mean accuracy';
-r{1,2} = 'max accuracy';
-r{1,3} = '#vx above 2% accuracy';
-r{1,4} = '#vx above 3% accuracy';
-r{1,5} = '#vx above 4% accuracy';
-r{2,1} = mean(corr_results.samples);
-r{2,2} = max(corr_results.samples);
-r{2,3} = length(find(corr_results.samples > 0.02));
-r{2,4} = length(find(corr_results.samples > 0.03));
-r{2,5} = length(find(corr_results.samples > 0.04));
+corr_results=cosmo_searchlight(all_scans,nbrhood,measure,opt); % ERRORS HERE
+corr_results.samples=corr_results.samples-(1/2);
+m_acc = mean(corr_results.samples)
+mx_acc = max(corr_results.samples)
 %% save and exit
 file_name = sprintf('Sub%d_MVPA_results',subID)
 output_fn=fullfile(mvpadir,file_name);
+
+
 
 wh = 1
 while exist([output_fn num2str(wh) '.nii'],'file') > 0
@@ -216,11 +185,10 @@ while exist([output_fn2 num2str(wh1) '.mat'],'file') > 0
 end
 save([output_fn2 num2str(wh1) '.mat'])
 
-
-
-%end
 % output_fn1 = fullfile(pwd,[ datestr(datetime) '_MVPA_outFile.nii'])
 % try
 % cosmo_map2fmri(corr_results, output_fn1);
 % catch
-%     cosmo_map2fmri(corr_results, output_fn);
+%     chistoosmo_map2fmri(corr_results, output_fn);
+mean(corr_results.samples(find(corr_results.samples > 0)))
+end
